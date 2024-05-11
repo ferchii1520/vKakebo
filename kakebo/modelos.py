1632574eba79fd9a1b2d39c58adc54235d99fec7
Fernanda Fuentes <fuentesdiazmafernanda@gmail.com>
2024-05-10 21:55:40 -0600

    Test de Guardar(Ingreso, Gasto) y Leer (Ingreso, Gasto

--- kakebo/modelos.py
+++ kakebo/modelos.py
@@ -1,5 +1,7 @@
 from datetime import date
 from enum import Enum
+import csv
+import os
 
 class Movimiento:
     def __init__(self, concepto, fecha, cantidad):
@@ -51,5 +53,26 @@ class CategoriaGastos(Enum):
 class Dao:
     def __init__(self, ruta):
         self.ruta = ruta
-        with open(self.ruta, "w", newline="") as f:
-            f.write("concepto,fecha,cantidad,categoria\n")
+        if not os.path.exists(self.ruta):
+            with open(self.ruta, "w", newline="") as f:
+                f.write("concepto,fecha,cantidad,categoria\n")
+
+    def grabar(self, movimiento):
+        with open(self.ruta, "a", newline="") as f:
+            writer = csv.writer(f, delimiter=",", quotechar='"')
+            if isinstance(movimiento, Ingreso):
+                #f.write(f"{movimiento.concepto},{movimiento.fecha},{movimiento.cantidad},\n") #Sin usar libreria csv
+                writer.writerow([movimiento.concepto,movimiento.fecha,movimiento.cantidad,""])
+            elif isinstance(movimiento, Gasto):
+                writer.writerow([movimiento.concepto,movimiento.fecha,movimiento.cantidad,movimiento.categoria.value])
+                #f.write(f"{movimiento.concepto},{movimiento.fecha},{movimiento.cantidad},{movimiento.categoria.value}\n") #Sin usar libreria csv
+    
+    def leer(self):
+        with open(self.ruta, "r") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                if row['categoria'] == "":
+                    variable = Ingreso(row['concepto'], date.fromisoformat(row['fecha']), float(row['cantidad']))
+                else:
+                    variable = Gasto(row["concepto"], date.fromisoformat(row["fecha"]), float(row["cantidad"]), CategoriaGastos(int(row["categoria"])))
+                return variable
--- tests/test_daos.py
+++ tests/test_daos.py
@@ -38,6 +38,24 @@ def test_guardar_Ingreso_Gasto():
         ingreso = f.readline()
         assert ingreso == "Un ingreso,1999-12-31,100,\n"
         registro = f.readline()
-        assert registro == "pizza,2000-01-01,150,4,\n"
+        assert registro == "pizza,2000-01-01,150,4\n"
         registro = f.readline()
         assert registro == ""
+
+def test_leer_Ingreso_Gasto():
+    ruta = "datos/test_movimientos.csv"
+    with open(ruta, "w", newline="") as f:
+        f.write("concepto,fecha,cantidad,categoria\n")
+        f.write("La luz,2020-04-02,1000.50,\n")
+        f.write("Sushi,2024-05-10,350.20,1\n")
+    
+    dao = Dao(ruta)
+
+    movimiento1 = dao.leer()
+    assert movimiento1 == Ingreso("La luz", date(2020, 4, 2), 1000.50)
+
+    movimiento2 = dao.leer()
+    assert movimiento2 == Gasto("Sushi", date(2024, 5, 10), 350.20, CategoriaGastos.NECESIDAD)
+
+    movimiento3 = dao.leer()
+    assert movimiento3 is None
